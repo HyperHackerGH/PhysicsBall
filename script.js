89b3a11db5405ac4161c866db20e85c8a4e810b6
HyperHacker ⠕ <108554192+HyperHackerGH@users.noreply.github.com>
2024-06-22 21:46:26 +0000

    d

--- script.js
+++ script.js
@@ -64,6 +64,14 @@ function main() {
             "ball"
         ])
 
+        const a = add([
+            circle(13),
+            pos(randi(0, width() - 16), randi(0, height() - 16)),
+            color(0, 255, 0),
+            area(),
+            "powerup"
+        ])
+
         loop(12, () => {
             var posx = randi(0, width() - 16)
             var posy = randi(0, height() - 16)
@@ -98,14 +106,6 @@ function main() {
                 }
             }, 200)
         })
-        
-        add([
-            circle(13),
-            pos(randi(0, width() - 16), randi(0, height() - 16)),
-            color(0, 255, 0),
-            area(),
-            "powerup"
-        ])
 
         loop(10, () => {
             if (powerup == false) {
